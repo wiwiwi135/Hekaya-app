76d1a11eb85a196964d398d9720d1ad02661b812
wiwiwi135 <m7md4gamez2009@gmail.com>
2026-02-08 17:51:16 +0200

    Add public/sw.js service worker file

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+// This is a simple service worker implementation
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker: Fetching', event.request.url);
+});
